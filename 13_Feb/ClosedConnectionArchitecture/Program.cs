84243c0cfb83aa23383b9e6f5b900c1552a50923
kamaljeet-01 <kamaljeetkamaljeet548@gmail.com>
2026-02-14 21:07:01 +0530

    Explored ADO.net  - open/ Closed Connection Architecture

---
+++ 13_Feb/ClosedConnectionArchitecture/Program.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+class Program
+{
+    static void Main()
+    {
+        string cs =
+        "Server=localhost;Database=TrainingDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        string sql = @"
+            SELECT EmployeeId, Department FROM Employees;
+            SELECT ProductId, ProductName, StockQty FROM Products;
+        ";
+
+        DataSet ds = new DataSet();
+
+        using SqlConnection con = new SqlConnection(cs);
+        con.Open();
+
+        SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+
+        // Naming tables
+        adapter.TableMappings.Add("Table", "Employees");
+        adapter.TableMappings.Add("Table1", "Products");
+
+        adapter.Fill(ds);
+
+        // ===== MANUAL CRUD COMMANDS FOR PRODUCTS =====
+
+        adapter.InsertCommand = new SqlCommand(
+            @"INSERT INTO Products(ProductName, StockQty)
+              VALUES(@ProductName, @StockQty)", con);
+
+        adapter.InsertCommand.Parameters.Add("@ProductName", SqlDbType.NVarChar, 100, "ProductName");
+        adapter.InsertCommand.Parameters.Add("@StockQty", SqlDbType.Int, 0, "StockQty");
+
+
+        adapter.UpdateCommand = new SqlCommand(
+            @"UPDATE Products
+              SET ProductName=@ProductName, StockQty=@StockQty
+              WHERE ProductId=@ProductId", con);
+
+        adapter.UpdateCommand.Parameters.Add("@ProductName", SqlDbType.NVarChar, 100, "ProductName");
+        adapter.UpdateCommand.Parameters.Add("@StockQty", SqlDbType.Int, 0, "StockQty");
+        adapter.UpdateCommand.Parameters.Add("@ProductId", SqlDbType.Int, 0, "ProductId");
+
+
+        adapter.DeleteCommand = new SqlCommand(
+            @"DELETE FROM Products WHERE ProductId=@ProductId", con);
+
+        adapter.DeleteCommand.Parameters.Add("@ProductId", SqlDbType.Int, 0, "ProductId");
+
+        // ===== CRUD OPERATIONS =====
+        DataTable products = ds.Tables["Products"];
+
+        // INSERT
+        var row = products.NewRow();
+        row["ProductName"] = "Headphones";
+        row["StockQty"] = 20;
+        products.Rows.Add(row);
+
+        // UPDATE
+        if (products.Rows.Count > 0)
+            products.Rows[0]["StockQty"] = 999;
+
+        // DELETE
+        if (products.Rows.Count > 1)
+            products.Rows[1].Delete();
+
+        // Update only Products table
+        adapter.Update(ds, "Products");
+
+        Console.WriteLine("Single adapter CRUD done.");
+    }
+}
--- 13_Feb/DataBaseConnectivity/Program.cs
+++ 13_Feb/DataBaseConnectivity/Program.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.Data.SqlClient;
-
+using System.Data;
 class Program
 {
     static void Main()
     {
-        string cs = "Server=localhost;Database=TrainingDB;Trusted_Connection=True;TrustServerCertificate=True;";
-        string sql = "INSERT INTO dbo.Employees (FullName, Department, Salary)VALUES ('Rahul Sharma', 'IT', 65000.00);";
-
+        string cs = "Server=localhost;Database=TrainingDB;Trusted_Connection=True;TrustServerCertificate=True;";    //------------------------ Should not be comment out
+        
+        //string dept = Console.ReadLine() ?? ""; //Make its default value as empty string
+        int id = int.Parse(Console.ReadLine());
+        string sql = @"Delete Employees where EmployeeId = @id";
+        //string sql = "INSERT INTO dbo.Employees (FullName, Department, Salary)VALUES ('Neha Sharma', 'HR', 65000.00);";
+        // string sql = @"UPDATE Employees SET FullName = 'sabya', Department = 'IT', Salary =69000 where EmployeeId = @id";
         using (var con = new SqlConnection(cs))
         using (var cmd = new SqlCommand(sql, con))
         {
-            con.Open();
+        //    // cmd.Parameters.AddWithValue("@dept",dept);
+             cmd.Parameters.AddWithValue("@id",id);
+             con.Open();
 
+
+        //------------------- Read the Data from DB------------------------------
             // using (var reader = cmd.BeginExecuteNonQuery())
             // {
             //     while (reader.Read())
@@ -24,8 +32,33 @@ class Program
             //         Console.WriteLine($"{id} | {name} | {dept} | {salary}");
             //     }
             // }
-            int rowsAffected = cmd.ExecuteNonQuery();
-            Console.WriteLine(rows == 1 ? " Inserted" : " Not inserted");
-        }
+
+        //------------------- Insert Data into DB --------------------------------------------------
+            // int RowsAffected = cmd.ExecuteNonQuery();
+            // Console.WriteLine(RowsAffected == 1 ? " Inserted" : " Not inserted");
+
+            
+        // ------------------ READ DATA USING PARAMETER --------------------------------------------
+            // using var r = cmd.ExecuteReader();
+            // while (r.Read())
+            // {
+            //     Console.WriteLine($"{r["EmployeeId"]} | {r["FullName"]} | {r["Salary"]}");
+            // }
+
+        // ---------------- UPDATE THE RECORD BASED ON EMPLOYEEID ----------------------------------
+
+            // int RowsAffected = cmd.ExecuteNonQuery();
+            // Console.WriteLine(RowsAffected == 1 ? " Updated" : " Not Updated");
+
+        // ---------------- DELECT THE RECORD FROM DB ----------------------------------------------
+            int RowsAffected = cmd.ExecuteNonQuery();
+            Console.WriteLine(RowsAffected == 1 ? " Deleted" : " Not Deleted"); 
+
+
+
+           con.Close();
+       }
+
+        
     }
 }
